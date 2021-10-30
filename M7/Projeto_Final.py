74a0b6842d31f4560e8f83a27e5c346a513cbc84
DOUGLAS CARVALHO PEREIRA <65472072+DouglasCarvalhoPereira@users.noreply.github.com>
2021-10-30 16:09:51 -0300

    Next Project Final

---
+++ M7/Projeto_Final.py
@@ -0,0 +1,28 @@
+#Projeto_Final
+# >>> Escrevendo um roteiro do Zero
+
+# 1° - Importante entender completamente o problema
+# 2° - Identificar as entradas e saídas necessárias
+# 3° - Realizar uma pesquisa para saber se já existe uma ferramenta ou módulo pronto para isso
+# 4° - Planejamento, saber quais dados são úteis para a solução, ordem de operações que preciso executar
+#      e como todas as peças se uniem.
+#      É comum escrever um documento detalhado, a declaração do problema as ferramentas que serão usadas
+#      para resolve-lo e o plano de ataque a uma solução.
+#      Pedir feedback de outros programadores garante novos pontos de vistas e soluções possíveis.
+# 5° - Escrita real do Script, verificando erros e possíveis melhoras.
+
+
+
+
+# 1 - Encontrar linhas de registros específicas em um arquivo de log. - ==> Expressões Regulares | regex101.com
+# 2 - Contar quantos erros são do mesmo tipo - ==> 1 Dicionário para...
+#       Classificar os dados no dicionários por critérios diferentes
+# 3 - Quantas informações e mensagens de erro existem para um determinado usuário - ==> Outro Dicionário para...
+#       Classificar os dados no dicionários por critérios diferentes
+
+# 4 - A saída do script deve ser um par de arquivos CSV, cada um deles contendo os nomes das colunas e dados na ordem que precisam ser apresentados.
+# 5 - Em seguida chamar o script CSV html.py para criar arquivos HTML com base em dados CSV
+#       O segredo é passar dois parametros para ele!
+#       1° - O nome do arquivo CSV a ser lido e o 
+#       2° - Nome do HTML gerado
+#       Pode ser feito usando Python ou Bash 
